e6a115f435d64cefbc23690b4889ec8345ef0e2c
ola-pt <57873134+ola-pt@users.noreply.github.com>
2020-07-01 20:16:21 -0400

    Update index.js
    Updated for the new methods of setting environment variables

--- index.js
+++ index.js
@@ -2,12 +2,13 @@
 // HMAC Duo v1 API Signing Pre-Request script.
 //
 // - Tory Berra 9/19/2018
+var CryptoJS = require("crypto-js");
 
 // You only need to mess with the following variables.
 const sKey = ""; // IE: "abc123def456hij789abc123def456hij789"
 const iKey = ""; // IE: "ABCDEF123456789"
-const host = request.url.split("/")[2].toLowerCase(); // IE: "api-xxxxxxxx.duosecurity.com"
-const path = request.url.replace(/^.*\/\/[^\/]+/, ''); // IE: "/admin/v1/users"
+const host = ""; // IE: "api-xxxxxxxx.duosecurity.com"
+const path = ""; // IE: "/admin/v1/users"
 
 //
 // End user variables.
@@ -26,7 +27,8 @@ for(var i = 0; i < paramKeys.length; i++) {
 var paramString = params.join('&');
 
 const content_type = 'application/x-www-form-urlencoded';
-const method = request.method.toUpperCase();
+//const method = request.method.toUpperCase();
+const method = "GET";
 
 // create canonical string
 var date = new Date().toUTCString();
@@ -38,9 +40,9 @@ var sig  = CryptoJS.HmacSHA1(canon, sKey);
 // Build Headers
 var auth =  `${iKey}:${sig}`;
 var base64 = Buffer.from(auth).toString('base64');
- 
+
 // Make sure you set these in the Headers tab of Postman
 // IE: Header: Date, Value: {{Date}}
-postman.setEnvironmentVariable('Date', date);
-postman.setEnvironmentVariable('Content-Type', content_type);
-postman.setEnvironmentVariable('Authorization', "Basic " + base64);
+pm.environment.set("Date", date);
+pm.environment.set("Content-Type", content_type);
+pm.environment.set("Authorization", "Basic " + base64);
